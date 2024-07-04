3b25940b037b0dc5e637a6e09e2b1381319efea1
nyxcrq <nyxcrq@gmail.com>
2024-07-04 14:42:39 +0700

    040724

---
+++ app/service/service.js
@@ -0,0 +1,39 @@
+import Navber from "../component/Navbar.js"
+
+export default function Home() {
+  return (
+
+    
+    <>
+    <Navber />
+    <h1 className="text-center mt-4">บริการของเรา</h1>
+      <div className="d-flex justify-content-center mt-2">
+
+      </div>
+
+      <div className="row mt-4">
+        <div className="col-md-4 mb-4">
+
+        </div>
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="img/img7.jpg" className="card-img-top" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title text-center">ไม่มีโฆษณา!!!!</h5>
+              <p className="card-text">
+               
+              </p>
+
+            </div>
+          </div>
+        </div>
+
+
+          </div>
+
+      
+    </>
+    
+
+  );
+}
